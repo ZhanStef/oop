310e4a066d466658c226aa78168623f3af8b4159
JeanStefan <jeanstefanovich@gmail.com>
2017-09-25 23:01:02 +0700

    lab1 spiral algorithm

--- oop lab1.cpp
+++ oop lab1.cpp
@@ -13,13 +13,13 @@ int main(int argc, char *argv[])
 {
     srand(time(0)); // ãåíåðàöèÿ ñëó÷àéíûõ ÷èñåë
     // äèíàìè÷åñêîå ñîçäàíèå äâóìåðíîãî ìàññèâà ÷èñåë íà 200 ýëåìåíòîâ
-	int N;
-	cout << "\nInpute size  of array: ";
-	cin >> N;
-	cout << endl;
-    int **ptrarray = new int* [N]; 
+    int N;
+    cout << "\nInpute size  of array: ";
+    cin >> N;
+    cout << endl;
+    int **ptrarray = new int* [N];
     for (int count = 0; count < N; count++)
-        ptrarray[count] = new int [N]; 
+        ptrarray[count] = new int [N];
     // çàïîëíåíèå ìàññèâà
     for (int count_row = 0; count_row < N; count_row++)
         for (int count_column = 0; count_column < N; count_column++)
@@ -38,17 +38,17 @@ int main(int argc, char *argv[])
     // delete first array
     for (int count = 0; count < 2; count++)
         delete []ptrarray[count];
-    
+
     return 0;
 }
 
 void right_diagonal(int **ptrarray, int N){
     cout << "right diagonal" << endl;
-	int *ptr_diagonal=new int[N*N];
+    int *ptr_diagonal=new int[N*N];
     int g=0, k=0;
     while(g<N){
             for(int i=0, j=N-g-1; i<g+1; i++, j++, k++) ptr_diagonal[k]=ptrarray[i][j];
-			g++;
+            g++;
     }
     for(int h=1;h<N;h++){
             for(int i=h, j=0; i<N; i++, j++, k++) ptr_diagonal[k]=ptrarray[i][j];
@@ -72,19 +72,19 @@ void left_diagonal(int *ptrarray[], int N){
 }
 
 void spiral_from_center(int *ptrarray[], int N){
-	cout << "\n center spiral" << endl;
+    cout << "\n center spiral" << endl;
     int *ptr=new int[N*N];
     cout << "\nСередина "<< N/2;
-    for(int i=0, step=1, dir=1, x=N/2, y=N/2; (x<N) && (y<N); -dir, step++){
-		int count_step=0;
-		while(count_step++<step){
-			ptr[i++]=ptrarray[x][y+dir];
-		}
-		count_step=0;
-		while(count_step++<step){
-			ptr[i++]=ptrarray[x+dir][y];
-		}
-	}
-	for(int h=0; h<N*N; h++)  cout << setw(4) << ptr[h];
-	delete ptr;
+    for(int i=0, step=1, dir=1, x=N/2, y=N/2, ptr[i++]=ptrarray[x][y]; (x<N) && (y<N); dir=-1*dir, step++){
+        int count_step=0;
+        while(count_step++<step){
+            ptr[i++]=ptrarray[x][y+dir];
+        }
+        count_step=0;
+        while(count_step++<step){
+            ptr[i++]=ptrarray[x+dir][y];
+        }
+    }
+    for(int h=0; h<N*N; h++)  cout << setw(4) << ptr[h];
+    delete ptr;
 }
